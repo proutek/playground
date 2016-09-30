37f42703a3f86d4928813d6485cc00449bd84c0c
chilly <chilly@email.cz>
2016-09-30 16:46:42 +0200

    experiment

---
+++ experimentjs/index.js
@@ -0,0 +1,6 @@
+
+var obj = {
+	item: 2 < 3 ? 2 : 3
+}
+
+console.log(obj.item);
---
+++ react-native-study/v09-thunk middleware-exercise/actions/index.js
@@ -0,0 +1,37 @@
+
+var decrease = function(){
+    return { type: 'DECREMENT' };
+}
+
+var increase = function(){
+    return { type: 'INCREMENT' };
+}
+
+var getSum = function(a, b){
+    return { type: 'SUM', a:a, b:b };
+}
+
+// ASYNC
+var asyncIncrease = function(dispatch, state) {
+  dispatch({type: 'INCREMENT_LOADING'});
+
+  _fakeServerApi.increaseCount(state.count.result,
+    function(data) {
+      dispatch({ type: 'INCREMENT'});
+    }
+  )
+}
+
+var getRandomImages = function(dispatch, state) {
+  dispatch({type: 'RANDOM_IMAGES_LOADING'});
+
+  var imgurAPI = "https://api.imgur.com/3/gallery/random/random/1";
+  $.getJSON(imgurAPI).done(function(data) {
+    var images = data.data.map(i => i.link);
+    dispatch({
+      type: 'RANDOM_IMAGES',
+      images
+    });
+  });
+
+}
